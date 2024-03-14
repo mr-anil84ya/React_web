a956a2b2adeaabd5f93fcfe3fc25c91f3128d971
Anil Kumar <79510192+mr-anil84ya@users.noreply.github.com>
2024-03-14 11:15:59 +0530

    some changes

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+function Footer() 
+{
+    return(
+        <div className="App">
+            <h1>Welcome To Footer Section</h1>
+        </div>
+    );
+    
+}
+export default Footer;
---
+++ src/header.js
@@ -0,0 +1,9 @@
+import './App.css';
+function Header(){
+    return(
+        <div className="App">
+            <h1>Hii Welcome Header Section</h1>
+        </div>
+    );
+}
+export default Header;
